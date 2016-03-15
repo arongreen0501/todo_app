f61ddb17a6c79fe0a9fbaaf7f34e8425cb610136
Aron Green <arongreen0501@gmail.com>
2016-03-15 15:45:28 +0000

    finito

---
+++ vendor/bundle/ruby/2.0.0/gems/sqlite3-1.3.11/ext/sqlite3/siteconf20160315-5712-s3jwwn.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20160315-5712-1frdn3e"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
