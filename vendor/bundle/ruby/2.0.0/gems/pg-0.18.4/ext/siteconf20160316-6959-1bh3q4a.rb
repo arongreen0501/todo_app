d98865cd6934bad7a214b1659049164b5468e4f6
Aron Green <arongreen0501@gmail.com>
2016-03-16 22:53:17 +0000

    server?

---
+++ vendor/bundle/ruby/2.0.0/gems/pg-0.18.4/ext/siteconf20160316-6959-1bh3q4a.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20160316-6959-j50ktt"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
---
+++ vendor/bundle/ruby/2.0.0/gems/sqlite3-1.3.11/ext/sqlite3/siteconf20160316-7023-1ad2ygj.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20160316-7023-1njd922"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
